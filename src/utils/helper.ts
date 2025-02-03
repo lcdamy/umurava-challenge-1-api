d690647003e960ffa7706900bcb629e4170e0005
lcdamy <zudanga@gmail.com>
2025-02-03 22:23:34 +0200

    update mock user data to use random user profile images for admin and participant roles

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,24 +3,24 @@ import { UserRole, UserPayload } from "../types";
 
 export function mockAdminUser(id: string) {
     const mockAdminUser: UserPayload[] = [
-        { id: "679f2df529592efbf6df223a", username: 'johnDoeAdmin', names: 'Johnathan Doe', email: "john.doe.admin@gmail.com", userRole: UserRole.Admin, profile_url: 'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg' },
-        { id: "679f2df529592efbf6df223b", username: 'janeSmithAdmin', names: 'Janet Smith', email: "jane.smith.admin@gmail.com", userRole: UserRole.Admin, profile_url: 'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg' }
+        { id: "679f2df529592efbf6df223a", username: 'johnDoeAdmin', names: 'Johnathan Doe', email: "john.doe.admin@gmail.com", userRole: UserRole.Admin, profile_url: 'https://randomuser.me/api/portraits/men/1.jpg' },
+        { id: "679f2df529592efbf6df223b", username: 'janeSmithAdmin', names: 'Janet Smith', email: "jane.smith.admin@gmail.com", userRole: UserRole.Admin, profile_url: 'https://randomuser.me/api/portraits/women/1.jpg' }
     ];
     return mockAdminUser.find(user => user.id === id);
 }
 
 export function mockParticipanteUser(id: string) {
     const mockParticipanteUser: UserPayload[] = [
-        { id: "679f2df529592efbf6df223c", username: 'johnDoeParticipant', names: 'Johnny Doe', email: "john.doe.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223d", username: 'janeSmithParticipant', names: 'Janelle Smith', email: "jane.smith.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223e", username: 'michaelBrownParticipant', names: 'Michael Brown', email: "michael.brown.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223f", username: 'emilyClarkParticipant', names: 'Emily Clark', email: "emily.clark.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223g", username: 'williamJohnsonParticipant', names: 'William Johnson', email: "william.johnson.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223h", username: 'oliviaMartinezParticipant', names: 'Olivia Martinez', email: "olivia.martinez.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223i", username: 'jamesAndersonParticipant', names: 'James Anderson', email: "james.anderson.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223j", username: 'sophiaTaylorParticipant', names: 'Sophia Taylor', email: "sophia.taylor.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223k", username: 'danielMooreParticipant', names: 'Daniel Moore', email: "daniel.moore.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
-        { id: "679f2df529592efbf6df223l", username: 'miaWhiteParticipant', names: 'Mia White', email: "mia.white.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png' },
+        { id: "679f2df529592efbf6df223c", username: 'johnDoeParticipant', names: 'Johnny Doe', email: "john.doe.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/men/2.jpg' },
+        { id: "679f2df529592efbf6df223d", username: 'janeSmithParticipant', names: 'Janelle Smith', email: "jane.smith.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/women/2.jpg' },
+        { id: "679f2df529592efbf6df223e", username: 'michaelBrownParticipant', names: 'Michael Brown', email: "michael.brown.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/men/3.jpg' },
+        { id: "679f2df529592efbf6df223f", username: 'emilyClarkParticipant', names: 'Emily Clark', email: "emily.clark.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/women/3.jpg' },
+        { id: "679f2df529592efbf6df223g", username: 'williamJohnsonParticipant', names: 'William Johnson', email: "william.johnson.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/men/4.jpg' },
+        { id: "679f2df529592efbf6df223h", username: 'oliviaMartinezParticipant', names: 'Olivia Martinez', email: "olivia.martinez.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/women/4.jpg' },
+        { id: "679f2df529592efbf6df223i", username: 'jamesAndersonParticipant', names: 'James Anderson', email: "james.anderson.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/men/5.jpg' },
+        { id: "679f2df529592efbf6df223j", username: 'sophiaTaylorParticipant', names: 'Sophia Taylor', email: "sophia.taylor.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/women/5.jpg' },
+        { id: "679f2df529592efbf6df223k", username: 'danielMooreParticipant', names: 'Daniel Moore', email: "daniel.moore.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/men/6.jpg' },
+        { id: "679f2df529592efbf6df223l", username: 'miaWhiteParticipant', names: 'Mia White', email: "mia.white.participant@gmail.com", userRole: UserRole.Participant, profile_url: 'https://randomuser.me/api/portraits/women/6.jpg' },
     ];
     return mockParticipanteUser.find(user => user.id === id);
 };
