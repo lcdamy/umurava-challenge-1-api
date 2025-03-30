ff9cc5f3bb1767142e9f8950714412f1a4765d83
lcdamy <zudanga@gmail.com>
2025-03-30 18:45:52 +0200

    refactor: remove unused frontend host variable from authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,12 +1,7 @@
-
-
 import bcrypt from "bcryptjs";
-
-
 
 export class AuthService {
 
-    private frontend_host = process.env.FRONTEND_URL ? process.env.FRONTEND_URL : 'http://localhost:3000';
 
     async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
         return bcrypt.compare(password, hashedPassword);
